d739eec08d315057be13a7b76b7d2956551c11a4
realandtrue <cattodoameow@proton.me>
2025-07-15 13:04:23 -0400

    cleaned some stuff up

--- inc/bot.hpp
+++ inc/bot.hpp
@@ -2,10 +2,20 @@
 #include <dpp/cluster.h>
 #include <dpp/dispatcher.h>
 #include <dpp/dpp.h>
+#include <string>
 
+template <std::string BotCommand>
 class Bot{
 public:
-	Bot(std::string tokenIn);
+	Bot(std::string tokenIn) : token(tokenIn), bot(tokenIn, dpp::i_default_intents | dpp::i_message_content) {
+		bot.on_log(dpp::utility::cout_logger());
+		bot.on_message_create([this](const dpp::message_create_t &in){
+				if(!in.msg.content.rfind(botCommand, 0)){ // if the bot command string is at the beginning of the message
+					Command(in, this->bot);
+				}
+			});
+	}
+
 	void Wait();
 private:
 	std::string token;
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include "../inc/bot.hpp"
+#include <string>
 int main(){
-	Bot bot("token");
+	const std::string botCommand = ".ping";
+	Bot<botCommand> bot("token");
 	bot.Wait();
 	return 0; // holy OOP
 }
