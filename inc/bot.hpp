5ff5d2a1c6739b02fbf529135e529ac87fad5a03
realandtrue <cattodoameow@proton.me>
2025-07-15 13:09:49 -0400

    Made bot take token from command line

--- inc/bot.hpp
+++ inc/bot.hpp
@@ -4,13 +4,13 @@
 #include <dpp/dpp.h>
 #include <string>
 
-template <std::string BotCommand>
 class Bot{
 public:
-	Bot(std::string tokenIn) : token(tokenIn), bot(tokenIn, dpp::i_default_intents | dpp::i_message_content) {
+	Bot(std::string tokenIn, std::string botCommand) : token(tokenIn), bot(tokenIn, dpp::i_default_intents | dpp::i_message_content) {
+		this->botCommand = botCommand;
 		bot.on_log(dpp::utility::cout_logger());
 		bot.on_message_create([this](const dpp::message_create_t &in){
-				if(!in.msg.content.rfind(botCommand, 0)){ // if the bot command string is at the beginning of the message
+				if(!in.msg.content.rfind(this->botCommand, 0)){ // if the bot command string is at the beginning of the message
 					Command(in, this->bot);
 				}
 			});
@@ -19,6 +19,7 @@ public:
 	void Wait();
 private:
 	std::string token;
+	std::string botCommand;
 	dpp::cluster bot;
 	static void Command(const dpp::message_create_t &in, dpp::cluster &bot);
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
 #include "../inc/bot.hpp"
 #include <string>
-int main(){
-	const std::string botCommand = ".ping";
-	Bot<botCommand> bot("token");
+int main(int argc, char **argv){
+	if(argc < 2){
+		std::cout << "Input bot token!" << std::endl;
+		exit(0);
+	}
+	std::string botCommand = ".ping";
+	std::string token = argv[1];
+	Bot bot(token, botCommand);
 	bot.Wait();
 	return 0; // holy OOP
 }
